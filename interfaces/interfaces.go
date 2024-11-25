1693afe76a5cbcc818d2c9ad852b24fe7621f44b
Nicolas Hampton <nicolashampton@DEEP-SPACE-NINE.local>
2024-11-24 23:54:17 -0500

    feat: Interface basics

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -14,3 +14,23 @@ func InterfaceLessons() {
 		fmt.Println("------------------------------------------------------------------")
 	}
 }
+
+// The interface defines BEHAVIOR
+type Writer interface {
+	Write([]byte) (int, error) // anything that writes fulfills the writer interface
+}
+
+// Interfaces are implicitly declared
+type ConsoleWriter struct {}
+
+// To implement the interface, just implement the listed behaviors
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
+}
+
+func interfaceBasics() {
+	// instead of a type here, we use an interface
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello Go!"))
+}
